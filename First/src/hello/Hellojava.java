0948683c2dd26db78c8e908ea8ce585eb492630d
admin <admin@DESKTOP-DL22QSN>
2020-08-03 11:12:13 +0900

    HelloJava proto

---
+++ First/src/hello/Hellojava.java
@@ -0,0 +1,26 @@
+/*
+ * DATE : 2020. 06. 26
+ * AUTHOR : 김춘향
+ * 설명 : 첫번째 자바 프로그램
+ * VER. 1.0
+ */
+
+
+package hello;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		//주석 : 코드를 설명. 개발자가 보기 위함.
+		System.out.println ("꺄흫");
+		/*
+		  
+		  이사이에 있는 공간은 전부 주석처리가 된다.
+		  이것은 여러줄
+		  주석
+		 
+		 * */
+	
+	}
+
+}
